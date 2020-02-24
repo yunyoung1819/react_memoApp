f32b535e1c59b3dbbdb1ce433e28da12edca12cc
yunyoung1819 <yunyoung1819@hanmail.net>
2020-02-25 00:52:15 +0900

    react simple memo note app commit

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,19 +1,20 @@
-// Header/index.js
 import React from 'react';
-import '/index.css';
+import './index.css';
 
 class Header extends React.Component {
     render() {
         return (
             <div className="header">
                 <div className="title">
-                    <span>Young's Memo Note</span>
+                    <span>Young's 심플노트</span>
                 </div>
                 <div className="buttons">
                     <button>추가</button>
                     <button>삭제</button>
                 </div>
             </div>
-        )
+        );
     }
 }
+
+export default Header;
