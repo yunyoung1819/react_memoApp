89c9f88930a1907d436ad36d362dac41e8afe11e
yunyoung1819 <yunyoung1819@hanmail.net>
2020-02-25 00:58:50 +0900

    test

---
+++ src/components/List/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './index.css';
+import ListItem from '../ListItem';
+
+class List extends React.Component {
+    render() {
+        return (
+            <div className="list">
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+                <ListItem />
+            </div>
+        );
+    }
+}
